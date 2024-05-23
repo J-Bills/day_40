292dc4ad466ddb7cff14a003dd4e1d1ab5b6a983
J-Bills <billsjamal@gmail.com>
2024-05-23 16:59:39 -0700

    finishing flight email and text alert app

--- flight-deals-end/main.py
+++ flight-deals-end/main.py
@@ -31,25 +31,26 @@ def main():
         else:
             print("That user already exists")
 
+    if sheet_data[0]["iataCode"] == "":
+        for row in sheet_data:
+            row["iataCode"] = flight_search.get_destination_code(row["city"])
+        data_manager.destination_data = sheet_data
+        data_manager.update_destination_codes()
 
+    tomorrow = datetime.now() + timedelta(days=1)
+    six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-    # if sheet_data[0]["iataCode"] == "":
-    #     for row in sheet_data:
-    #         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    #     data_manager.destination_data = sheet_data
-    #     data_manager.update_destination_codes()
-
-    # tomorrow = datetime.now() + timedelta(days=1)
-    # six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
-
-    # for destination in sheet_data:
-    #     flight = flight_search.check_flights(
-    #         ORIGIN_CITY_IATA,
-    #         destination["iataCode"],
-    #         from_time=tomorrow,
-    #         to_time=six_month_from_today
-    #     )
-    #     if flight.price < destination["lowestPrice"]:
-    #         notification_manager.send_sms(
-    #             message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-    #         )
+    for destination in sheet_data:
+        flight = flight_search.check_flights(
+            ORIGIN_CITY_IATA,
+            destination["iataCode"],
+            from_time=tomorrow,
+            to_time=six_month_from_today
+        )
+        if flight == None:
+            continue
+        
+        if flight.price < destination["lowestPrice"]:
+            notification_manager.send_sms(
+                message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}.\n'
+                Flight has {flight.stop_over}, via {flight.via_city}.")
